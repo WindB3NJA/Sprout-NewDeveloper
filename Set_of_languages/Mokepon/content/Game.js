67b212f8007bc2878111ac11e16113d1aabb4eba
WindB3NJA <benjaminzamora695@gmail.com>
2023-09-05 11:25:07 -0300

    Se agrega funciones que se activan al activar o clickear un boton

--- Set_of_languages/Mokepon/content/Game.js
+++ Set_of_languages/Mokepon/content/Game.js
@@ -1,3 +1,24 @@
+function gameLoading() {
+    let buttonSelectMokepon = document.getElementById('button-select-mokepon')
+    buttonSelectMokepon.addEventListener('click', PlayerChoise)
+}
 
-    player_healt = 3;
-    Rival_healt = 3;
+function PlayerChoise() {
+    let choiseRaditen = document.getElementById('Radinten')
+    let choiseMizutana = document.getElementById('Mizutana')
+    let choiseHanize = document.getElementById('Hanize')
+
+    if(choiseRaditen.checked) {
+        alert('Haz seleccionado a 🔥Raditen')
+    }
+    else if(choiseMizutana.checked) {
+        alert('Haz seleccionado a 💧Mizutana')
+    }
+    else if(choiseHanize.checked) {
+        alert('Haz seleccionado a 🌱Hanize')
+    }else{
+        alert('Seleccione un Mokepon Porfavor')
+    }
+} 
+
+window.addEventListener('load', gameLoading)
