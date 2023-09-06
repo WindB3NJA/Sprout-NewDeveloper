791d0cc6bbaac36f7a82c6c94c482ec86aa66ddf
WindB3NJA <benjaminzamora695@gmail.com>
2023-09-06 08:57:36 -0300

    Se agregan assents para los Mokepones y icono para la pagina web

--- Set_of_languages/Mokepon/content/Game.js
+++ Set_of_languages/Mokepon/content/Game.js
@@ -7,14 +7,18 @@ function PlayerChoise() {
     let choiseRaditen = document.getElementById('Radinten')
     let choiseMizutana = document.getElementById('Mizutana')
     let choiseHanize = document.getElementById('Hanize')
+    let PlayerMokepon = document.getElementById("player-mokepon")
 
     if(choiseRaditen.checked) {
+        PlayerMokepon.innerHTML = '🔥Raditen'
         alert('Haz seleccionado a 🔥Raditen')
     }
     else if(choiseMizutana.checked) {
+        PlayerMokepon.innerHTML = '💧Mizutana'
         alert('Haz seleccionado a 💧Mizutana')
     }
     else if(choiseHanize.checked) {
+        PlayerMokepon.innerHTML = "🌱Hanize"
         alert('Haz seleccionado a 🌱Hanize')
     }else{
         alert('Seleccione un Mokepon Porfavor')
