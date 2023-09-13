34a9034074bbd75459012b6b48c1f26df0c562be
WindB3NJA <benjaminzamora695@gmail.com>
2023-09-12 22:23:10 -0300

    Se agrega la funcionalidad de reniciar el juego y el desactivar los botones al terminar la partida

--- Set_of_languages/Mokepon/content/Game.js
+++ Set_of_languages/Mokepon/content/Game.js
@@ -1,34 +1,28 @@
+//Jugador
 let playerAtack
 let playerBattleLife = 3 ;
+//Rival
 let rivalAtack
 let rivalBattleLife = 3 ;
 //se Incializa el JavaScript con una funcion que espera al usuario que apriente una opcion para disparar el juego
 function gameLoading() {
+    //Boton para verificar si el usuario al haber tocado ya click en mokepon y al boton de confirmar seleccione el mokepon
     let buttonSelectMokepon = document.getElementById("button-select-mokepon") ;
     buttonSelectMokepon.addEventListener("click", PlayerChoise)
-    
+    //Botones de seleccion de ataque
     let playerButtonFire = document.getElementById("button-fire") ;
     playerButtonFire.addEventListener("click", PlayerButtonFire)
     let playerButtonWater = document.getElementById("button-water") ;
     playerButtonWater.addEventListener("click", PlayerButtonWater)
     let playerButtonPlant = document.getElementById("button-plant") ;
     playerButtonPlant.addEventListener("click", PlayerButtonPlant)
+    //Reiniciar el juego
+    let gameButtonRestart = document.getElementById("restart")
+    gameButtonRestart.addEventListener("click", gameRestart)
 }
-//Desicion de la maquina para elegir que a la criatura mokepon que utliza se ejecuta despues de 
-function randomMokeponChoise() {
-    let rivalMokeponChoise = num_random(1,3) ;
-    let RivalMokepon = document.getElementById("rival-mokepon-name") ;
-
-    if (rivalMokeponChoise == 1) {
-        RivalMokepon.innerHTML = "🔥Raditen"
-        alert("Tú rival ha seleccionado a 🔥Raditen")
-    } else if (rivalMokeponChoise == 2) {
-        RivalMokepon.innerHTML = "💧Mizutana"
-        alert("Tú rival ha seleccionado a 💧Mizutana")
-    } else if (rivalMokeponChoise == 3) {
-        RivalMokepon.innerHTML = "🌱Hanize"
-        alert("Tú rival ha seleccionado a 🌱Hanize")
-    }
+//Funcion para reiniciar el juego
+function gameRestart(){
+    location.reload()
 }
 //Generador de numeros aleatorios
 function num_random(min,max){
@@ -37,8 +31,12 @@ function num_random(min,max){
 //Selecion del jugaro a la criatura llamada mokepon
 function PlayerChoise() {
     //Reinicio de vidas
+    let playerStringLife = document.getElementById("player-life");
+    let rivalStringLife = document.getElementById("rival-life");
     playerBattleLife = 3 ;
+    playerStringLife.innerHTML = playerBattleLife ;
     rivalBattleLife = 3 ;
+    rivalStringLife.innerHTML = rivalBattleLife ;
     //elecion del Mokepon
     let choiseRaditen = document.getElementById("Radinten") ;
     let choiseMizutana = document.getElementById("Mizutana") ;
@@ -58,8 +56,25 @@ function PlayerChoise() {
         alert("Seleccione un Mokepon Porfavor")
     }
     //Eleccion de el rival
-    randomMokeponChoise()
+    rivalMokeponChoise()
 }
+//Desicion de la maquina para elegir que a la criatura mokepon que utliza se ejecuta despues de 
+function rivalMokeponChoise() {
+    let rivalMokeponChoise = num_random(1,3) ;
+    let RivalMokepon = document.getElementById("rival-mokepon-name") ;
+
+    if (rivalMokeponChoise == 1) {
+        RivalMokepon.innerHTML = "🔥Raditen"
+        alert("Tú rival ha seleccionado a 🔥Raditen")
+    } else if (rivalMokeponChoise == 2) {
+        RivalMokepon.innerHTML = "💧Mizutana"
+        alert("Tú rival ha seleccionado a 💧Mizutana")
+    } else if (rivalMokeponChoise == 3) {
+        RivalMokepon.innerHTML = "🌱Hanize"
+        alert("Tú rival ha seleccionado a 🌱Hanize")
+    }
+}
+//Botones de ataque del jugador
 //Boton de fuego
 function PlayerButtonFire() {
     playerAtack = "Fuego 🔥"
@@ -75,7 +90,7 @@ function PlayerButtonPlant() {
     playerAtack = "Planta 🌱"
     rivalAtackChoise()
 }
-//Ataque de el rival
+//Ataque del rival
 function rivalAtackChoise() {
     rivalAtackRandomChoise = num_random(1,3) ;
     
@@ -86,10 +101,10 @@ function rivalAtackChoise() {
     } else if (rivalAtackRandomChoise == 3){
         rivalAtack = "Planta 🌱"
     }
-    gameBattleStatus()
+    gameMesaggeStatus()
 }
 //generador de mensajes de la batalla
-function gameBattleStatus(){
+function gameMesaggeStatus() {
     let gameBattleResults = "" 
     if (playerAtack == rivalAtack) {
         gameBattleResults = "¡Un empate de Elementos! 😲"
@@ -112,16 +127,49 @@ function gameBattleStatus(){
         gameBattleResults = "Leña para el asado 🔥, ¡Haz Ganado esta ronda!"
         rivalBattleLife--
     } 
-
-    let gameMessageAtack  = document.getElementById("messages");
+    let gameMessageAtack  = document.getElementById("battle-messages");
     let BattleString = document.createElement("p");
     BattleString.innerHTML = "Tú mascota Ataco con "+ playerAtack +", La mascota del Rival Ataco con "+ rivalAtack +" - "+ gameBattleResults
     gameMessageAtack.appendChild(BattleString);
-    //Sistema de vidas 
+    //Sistema de vidas en HTML
     let playerStringLife = document.getElementById("player-life");
-    playerStringLife.innerHTML = playerBattleLife
     let rivalStringLife = document.getElementById("rival-life");
-    rivalStringLife.innerHTML = rivalBattleLife
+    //Comparador de vidas y mensajes
+    if(playerBattleLife == 0 || rivalBattleLife == 0) {
+        playerStringLife.innerHTML = playerBattleLife
+        rivalStringLife.innerHTML = rivalBattleLife
+        gameBattleFinalResults()
+    } else {
+        playerStringLife.innerHTML = playerBattleLife
+        rivalStringLife.innerHTML = rivalBattleLife
+    }
+
 }
+//Desactivador de botones de ataque
+function buttonDisableAtack(){
+    let playerButtonFire = document.getElementById("button-fire") ;
+    playerButtonFire.disabled = true
+    let playerButtonWater = document.getElementById("button-water") ;
+    playerButtonWater.disabled = true
+    let playerButtonPlant = document.getElementById("button-plant") ;
+    playerButtonPlant.disabled = true
+}
+//Comparador de vidas 
+function gameBattleFinalResults() {
+    let BattleResults  = document.getElementById("battle-Results");
+    let BattleResultsString = document.createElement("p");
+    if (playerBattleLife == 0){
+        //Perdiste
+        buttonDisableAtack()
+        BattleResultsString.innerHTML = "😞Haz Perdido la Batalla😞, con " + playerBattleLife + " Puntos vitales vs los Puntos "+ rivalBattleLife +" vitales del enemigo"
+        BattleResults.appendChild(BattleResultsString);
+    } else if (rivalBattleLife == 0){
+        //ganaste
+        buttonDisableAtack()
+        BattleResultsString.innerHTML = "🎊Haz Ganado la Batalla🎊, con " + playerBattleLife + " Puntos vitales vs los Puntos "+ rivalBattleLife +" vitales del enemigo"
+        BattleResults.appendChild(BattleResultsString);
+    }
+}
+
 //Se espera a que la pagina carge para luego correr el JavaScript
 window.addEventListener("load", gameLoading)
